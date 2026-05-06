22afbef02da9cfb4407b1cb867c22d15e0519e11
durgaganesh-G <gorreladurgaganesh@gmail.com>
2026-05-06 17:44:54 +0530

    Add various SQL queries for employee data analysis

--- week1/day1/basic_queries.sql
+++ week1/day1/basic_queries.sql
@@ -1,1 +1,49 @@
 
+-- Basic Queries
+SELECT * FROM Employee;
+SELECT name, salary FROM Employee;
+SELECT * FROM Employee WHERE age > 30;
+SELECT name FROM Department;
+SELECT * FROM Employee WHERE department_id = (SELECT department_id FROM Department WHERE name = 'IT');
+
+-- String Matching Queries
+SELECT * FROM Employee WHERE name LIKE 'J%';
+SELECT * FROM Employee WHERE name LIKE '%e';
+SELECT * FROM Employee WHERE name LIKE '%a%';
+SELECT * FROM Employee WHERE LENGTH(name) = 9;
+SELECT * FROM Employee WHERE name LIKE '_o%';
+
+-- Date Queries
+SELECT * FROM Employee WHERE YEAR(hire_date) = 2020;
+SELECT * FROM Employee WHERE MONTH(hire_date) = 1;
+SELECT * FROM Employee WHERE hire_date < '2019-01-01';
+SELECT * FROM Employee WHERE hire_date >= '2021-03-01';
+SELECT * FROM Employee WHERE hire_date >= DATE_SUB(CURDATE(), INTERVAL 2 YEAR);
+
+-- Aggregate Queries
+SELECT SUM(salary) FROM Employee;
+SELECT AVG(salary) FROM Employee;
+SELECT MIN(salary) FROM Employee;
+SELECT department_id, COUNT(*) FROM Employee GROUP BY department_id;
+SELECT department_id, AVG(salary) FROM Employee GROUP BY department_id;
+
+-- Group By Queries
+SELECT department_id, SUM(salary) FROM Employee GROUP BY department_id;
+SELECT department_id, AVG(age) FROM Employee GROUP BY department_id;
+SELECT YEAR(hire_date), COUNT(*) FROM Employee GROUP BY YEAR(hire_date);
+SELECT department_id, MAX(salary) FROM Employee GROUP BY department_id;
+SELECT department_id FROM Employee GROUP BY department_id ORDER BY AVG(salary) DESC LIMIT 1;
+
+-- Having Queries
+SELECT department_id FROM Employee GROUP BY department_id HAVING COUNT(*) > 2;
+SELECT department_id FROM Employee GROUP BY department_id HAVING AVG(salary) > 55000;
+SELECT YEAR(hire_date) FROM Employee GROUP BY YEAR(hire_date) HAVING COUNT(*) > 1;
+SELECT department_id FROM Employee GROUP BY department_id HAVING SUM(salary) < 100000;
+SELECT department_id FROM Employee GROUP BY department_id HAVING MAX(salary) > 75000;
+
+-- Order By Queries
+SELECT * FROM Employee ORDER BY salary ASC;
+SELECT * FROM Employee ORDER BY age DESC;
+SELECT * FROM Employee ORDER BY hire_date ASC;
+SELECT * FROM Employee ORDER BY department_id, salary;
+SELECT department_id, SUM(salary) FROM Employee GROUP BY department_id ORDER BY SUM(salary);
